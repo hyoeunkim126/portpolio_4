82d1283e9fff1107f4f0ad220da7c475d9e817a8
hyoeunkim126 <hyoeun126@naver.com>
2021-12-17 15:45:35 +0900

    Update leica.js

--- js/leica.js
+++ js/leica.js
@@ -1,40 +1,52 @@
 var currentScrollTop=0;
-        $(document).ready(function(){
-                    scrollController();
-                    $(window).scroll(function(){
-                        scrollController();
-                        });
-                    });
-                    function scrollController(){
-                    currentScrollTop = $(window).scrollTop();
-                        if(currentScrollTop > 900) { 
-                            $("header").addClass("active");
-                            $(".nav_link").css("color", "#333");
-                            }else{
-                            $("header").removeClass("active");
-                            $(".nav_link").css("color", "#eee");
-                                }
-                            }
+$(document).ready(function(){
+            scrollController();
+            $(window).scroll(function(){
+                scrollController();
+                });
+            });
+            function scrollController(){
+            currentScrollTop = $(window).scrollTop();
+                if(currentScrollTop > 900) { 
+                    $("header").addClass("active");
+                    $(".nav_link").css("color", "#333");
+                    }else{
+                    $("header").removeClass("active");
+                    $(".nav_link").css("color", "#eee");
+                        }
+                    }
+
+
+ $(function(){
+            var slidemenu = $("#slide_menu ul li");
+            var contents = $("#slider .s_photo1 ul li");
+
+            slidemenu.click(function(){
+                var target=$(this);
+                var index=target.index();
+
+                var photo  = contents.eq(index);
+                contents.not(photo).fadeOut("fast");
+                photo.fadeIn();
+            });
+        });
+
+
+
+                            
+
         var i=0;
         function slide(){
             i++;
             if(i>$('.slide li:last').index()){
                 i=0;
             }
+            $('.slide li').fadeOut();
             $('.slide li').eq(i).stop().fadeIn('slow');
             $('.slide li').eq(i-1).stop().fadeOut();
-
+        
         };
         setInterval(slide,3000);
-        
-        var slidemenu = $("#slide_menu ul li");
-        var contents = $("#slider .s_photo1 ul li");
-        $(function(){
-            slidemenu.click(function(){
-                var target=$(this);
-                var index=target.index();
 
-                var photo  = contents.eq(index);
-                photo.fadeIn(2000);
-            });
-        });
+        
+       
