4b089edfc20892781b8fb7d871e94f4729eadc5d
hyoeunkim126 <93234816+hyoeunkim126@users.noreply.github.com>
2021-12-20 17:45:57 +0900

    Create leica.js

---
+++ js/leica.js
@@ -0,0 +1,61 @@
+var currentScrollTop=0;
+$(document).ready(function(){
+            scrollController();
+            $(window).scroll(function(){
+                scrollController();
+                });
+            });
+            function scrollController(){
+            currentScrollTop = $(window).scrollTop();
+                if(currentScrollTop > 900) { 
+                    $("header").addClass("active");
+                    $(".nav_link").css("color", "#333");
+                    }else{
+                    $("header").removeClass("active");
+                    $(".nav_link").css("color", "#eee");
+                        }
+                    }
+
+
+ $(function(){
+            var slidemenu = $("#slide_menu ul li");
+            var contents = $("#slider .s_photo1 ul li");
+
+            slidemenu.click(function(){
+                var target=$(this);
+                var index=target.index();
+
+                var photo  = contents.eq(index);
+                contents.not(photo).fadeOut("fast");
+                photo.fadeIn();
+            });
+        });
+
+
+
+                            
+
+        var i=0;
+        function slide(){
+            i++;
+            if(i>$('.slide li:last').index()){
+                i=0;
+            }
+            $('.slide li').fadeOut();
+            $('.slide li').eq(i).stop().fadeIn('slow');
+            $('.slide li').eq(i-1).stop().fadeOut();
+        
+        };
+        setInterval(slide,3000);
+
+        
+$(function(){
+    $(".pro_content").ulslide({
+        statusbar:true,
+        affect:'slide',
+        axis: 'x',
+        mousewheel:true,
+        duration:400,
+        autoslide:3000
+    });
+});       
